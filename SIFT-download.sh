29319a63e73950c6a00cff889ccc13c1b56b9626
privsearch <privsearch@yahoo.com>
2024-10-01 02:06:24 +0000

    download sh

--- SIFT-download.sh
+++ SIFT-download.sh
@@ -12,28 +12,26 @@ echo "Downloading the SIFT dataset to $OUTPUT_DIR..."
 # Create the output directory if it doesn't exist
 mkdir -p "$OUTPUT_DIR"
 
-# Download the SIFT1B dataset using wget
-#wget -P "$OUTPUT_DIR" "$DATA_URL"
-
 # Download the queries using wget
-#wget -P "$OUTPUT_DIR" "$QUERIES_URL"
+echo "Downloading the queries..."
+wget -P "$OUTPUT_DIR" "$QUERIES_URL"
 
 # Download the groundtruth using wget
-#wget -P "$OUTPUT_DIR" "$GROUNDTRUTH_URL"
+echo "Downloading the groundtruth..."
+wget -P "$OUTPUT_DIR" "$GROUNDTRUTH_URL"
+
+# Download the SIFT1B dataset using wget
+echo "Downloading the SIFT1B dataset...expecting 92GB download"
+wget -P "$OUTPUT_DIR" "$DATA_URL"
 
 # Extract the dataset (if it's in a compressed format)
 
 # uncompress the gz file
+echo "Extracting the SIFT1B dataset...expecting ~230GB disk space"
 gunzip -v "$OUTPUT_DIR/bigann_base.bvecs.gz"
+
+echo "Extracting the queries and groundtruth..."
 gunzip -v "$OUTPUT_DIR/bigann_query.bvecs.gz"
 tar -xf "$OUTPUT_DIR/bigann_gnd.tar.gz" -C "$OUTPUT_DIR"
-
-# Extract the dataset (if it's in a compressed format)
-# Uncomment the following line if needed
-# tar -xf "$OUTPUT_DIR/sift1b.tar.gz" -C "$OUTPUT_DIR"
-
-# Optional: Remove the downloaded archive file
-# Uncomment the following line if needed
-# rm "$OUTPUT_DIR/sift1b.tar.gz"
 
 echo "Dataset downloaded successfully!"
